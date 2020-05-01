2ddb7275fcbe6e3a95bc4dc58c5ee300cd58e799
Artur <hello@arturneufeld.com>
2020-05-01 17:24:56 +0200

    dist

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="sc-player__buttons" :style=" trackIndex !== 0 ? 'text-align: left' : ''">
-  <PrevButton v-if="hasPlaylist && trackIndex !== 0" :set-current-track="setCurrentTrack" :soundcloud="soundcloud" :sounds="sounds" :track-index="trackIndex" />
+<div class=" sc-buttons-wrapper">
+  <PrevButton :class="hasPlaylist && trackIndex !== 0  ? '' : 'sc-disable'" :set-current-track="setCurrentTrack" :soundcloud="soundcloud" :sounds="sounds" :track-index="trackIndex" />
   <PlayButton :soundcloud="soundcloud" :track-playing="trackPlaying" />
-  <NextButton v-if="hasPlaylist && trackIndex !== sounds.length-1" :set-current-track="setCurrentTrack" :soundcloud="soundcloud" :sounds="sounds" :track-index="trackIndex" />
+  <NextButton :class="hasPlaylist && trackIndex !== sounds.length-1 ? '' : 'sc-disable'" :set-current-track="setCurrentTrack" :soundcloud="soundcloud" :sounds="sounds" :track-index="trackIndex" />
 </div>
 </template>
 
--- src/components/PlayButton.vue
+++ src/components/PlayButton.vue
@@ -1,5 +1,17 @@
 <template>
-<button :class="buttonClasses" type="button" @click="playPause" />
+<!-- <button :class="buttonClasses" type="button" @click="playPause" /> -->
+
+<div class="sifi tag sc-button" @click="playPause">
+  <svg v-if="isPaused" width="32" height="23" viewBox="0 0 32 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.1846 17.4085V6.45581L22.529 11.6279L12.1846 17.4085Z" fill="rgba(236, 236, 236,.75)" />
+  </svg>
+
+  <svg v-else width="32" height="23" viewBox="0 0 32 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="11.5273" y="6.28391" width="3.30342" height="10.4322" fill="rgba(236, 236, 236,.75)" />
+    <rect x="17.1689" y="6.28391" width="3.30342" height="10.4322" fill="rgba(236, 236, 236,.75)" />
+  </svg>
+
+</div>
 </template>
 
 <script>
@@ -7,6 +19,11 @@ import classnames from 'classnames';
 
 export default {
   name: "PlayButton",
+  data() {
+    return {
+      isPaused: true
+    }
+  },
   props: {
     soundcloud: {
       type: Object,
@@ -28,8 +45,10 @@ export default {
     playPause() {
       this.soundcloud.isPaused((paused) => {
         if (paused) {
+          this.isPaused = false;
           this.soundcloud.play();
         } else {
+          this.isPaused = true;
           this.soundcloud.pause();
         }
       });
